997e2bd4f3b9f2d78337e62ba7df23743c1d0b93
Shiva Sundara Balaji J M <sivasundarabalaji@gmail.com>
2026-07-26 12:19:03 +0530

    Migration imported

---
+++ app/Models/Sale.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    //
+    protected $fillable = [
+        "customer_name",
+        "invoice_no",
+        "sale_date",
+        "total_amount"
+    ];
+}
